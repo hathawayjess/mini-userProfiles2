1feeae9b89b499793ec52b7092b3135971aa7211
Jessica Hathaway <hathaway.jess@gmail.com>
2016-02-17 12:00:24 -0700

    Finished

--- controller.js
+++ controller.js
@@ -3,9 +3,10 @@ var app = angular.module('userProfiles');
 app.controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	mainService.getUsers().then(function(response) {
+  		$scope.users = response.data.data;  //data is a child of response, and also has a child named data
+  	});                                     //the second data is where our user information lives
   }
-
 
   $scope.getUsers();
 
